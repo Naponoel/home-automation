896e73a111449dcc93a961f18f3af51dd17d5093
Naponoel <lekileo@gmail.com>
2020-06-15 22:41:19 +0200

    sqa_test has Korisnik, Kontroler and Pin SQL class

--- sqa_test.py
+++ sqa_test.py
@@ -1,8 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
+from sqlalchemy.orm import sessionmaker, relationship
 
 DATABASE_URI = 'postgres+psycopg2://postgres:admin@localhost:5432/leon_testing'
 Base = declarative_base()
@@ -17,13 +16,23 @@ class Korisnik(Base):
     def __repr__(self):
         return f"User(username: '{self.username}', password: '{self.password}')"
 
+
+class Kontroler(Base):
+    __tablename__ = 'kontroleri'
+    id = Column(Integer, primary_key=True)
+    children = relationship("Pin", back_populates="parent")
+
+
+class Pin(Base):
+    __tablename__ = 'pinovi'
+    id = Column(Integer, primary_key=True)
+    parent_id = Column(Integer, ForeignKey('kontroleri.id'))
+    parent = relationship("Kontroler", back_populates="children")
+
+
 engine = create_engine(DATABASE_URI, echo=True)
 Base.metadata.create_all(engine)
 
-
-djuro = Korisnik(username='djuro', password='blaaa')
-
 Session = sessionmaker(bind=engine)
 session = Session()
-session.add(djuro)
-session.commit()
+# session.commit()
